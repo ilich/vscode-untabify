d3b22ccc8a76878a8ca4a7cd3289919b64fa107b
Ilya Verbitskiy <iverbitskiy@gmail.com>
2015-12-07 10:48:53 -0600

    Built extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,104 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode'; 
+import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "Untabify" is now active!'); 
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	var disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	var tabify = vscode.commands.registerTextEditorCommand("tabify", (editor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
+		var extension = new Untabify(editor);
+		extension.execute(COMMAND.TABIFY);
 	});
-	
-	context.subscriptions.push(disposable);
+
+	var untabify = vscode.commands.registerTextEditorCommand("untabify", (editor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
+		var extension = new Untabify(editor);
+		extension.execute(COMMAND.UNTABIFY);
+	});
+
+	context.subscriptions.push(tabify);
+	context.subscriptions.push(untabify);
+}
+
+enum COMMAND {
+	TABIFY,
+	UNTABIFY
+};
+
+const DEFAULT_SPACES_PER_TAB: number = 4;
+
+class Untabify {
+	private _editor: vscode.TextEditor;
+
+	constructor(editor: vscode.TextEditor) {
+		this._editor = editor;
+	}
+
+	execute(command: COMMAND) {
+		if (this._editor === undefined) {
+			return;
+		}
+
+		var config = vscode.workspace.getConfiguration("editor");
+		var tabSize = config.get("tabSize", "auto");
+		if (tabSize === "auto") {
+			tabSize = DEFAULT_SPACES_PER_TAB.toString();
+		}
+
+		vscode.window.showInputBox({
+			prompt: "How many spaces are in a single tab?",
+			value: tabSize
+		}).then((spacesPerTab) => {
+			if (spacesPerTab === undefined) {
+				return;
+			}
+
+			var spaces = parseInt(spacesPerTab);
+			if (isNaN(spaces)) {
+				spaces = DEFAULT_SPACES_PER_TAB;
+			}
+
+			switch(command) {
+				case COMMAND.TABIFY:
+					this.tabify(spaces);
+					break;
+
+				case COMMAND.UNTABIFY:
+					this.untabify(spaces);
+					break;
+			}
+		});
+	}
+
+	private tabify(spaces: number) {
+		var re = new RegExp(`[ ]{${spaces}}`, "ig");
+		var repl = "\t";
+
+		this.updateContent(re, repl);
+	}
+
+	private untabify(spaces: number) {
+		var re = /\t/ig;
+		var repl = "";
+		for (let i = 0; i < spaces; i++) {
+			repl += " ";
+		}
+
+		this.updateContent(re, repl);
+	}
+
+	private updateContent(regex: RegExp, replacement: string) {
+		if (this._editor === undefined) {
+			return;
+		}
+
+		var e = this._editor;
+		var lines = e.document.lineCount;
+		var lastLine = e.document.lineAt(lines - 1);
+		var start = new vscode.Position(0, 0);
+		var end = new vscode.Position(lastLine.lineNumber, lastLine.text.length);
+		var selection = new vscode.Selection(start, end);
+		var text = e.document.getText();
+		var fixedText = text.replace(regex, replacement);
+
+		e.edit((builder) => {
+			builder.replace(selection, fixedText);
+		});
+	}
 }
